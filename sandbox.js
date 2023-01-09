8f028cc02a046c2ee4c6b694b684b5492c59d26a
TheBigH50 <HFangmeier@outlook.com>
2023-01-09 12:09:18 -0600

    clean up and add more logs

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,27 @@
+// Declare the variables
 let myExample = "Valuable";
 let somethingElse = "Gold Bars";
+let combination = "";
 
+// Write the function
 function teachingPeople(paramOne, param2) {
-let combination = paramOne + param2;
-console.log(`Inside the function, ${paramOne} + ${param2} = ${combination}`);
-return combination;
+  // Assigning a new value to combination with the 2 parameter values (passing variable values as arguments)
+  combination = paramOne + param2;
+  // Log with 2 parameters and the new value
+  console.log(
+    `Inside the function, paramOne: ${paramOne} + param2: ${param2} = combination: ${combination}`
+  );
+  // Logs ==> Inside the function, paramOne: Valuable + param2: Gold Bars = combination: ValuableGold Bars
 }
+
 //Call the function
 teachingPeople(myExample, somethingElse);
 
-console.log(`These variables no longer exist after the function, ${paramOne} + ${param2} = ${combination}`);
-console.log(`These variables all exist outside the function, ${myExample} + ${somethingElse} = ${combination}`);
+console.log(
+  `These variables all exist outside the function, myExample: ${myExample} + somethingElse: ${somethingElse} = combination: ${combination}`
+);
+// Logs ==> These variables all exist outside the function, myExample: Valuable + somethingElse: Gold Bars = combination: ValuableGold Bars
+console.log(
+  `These variables no longer exist after the function, paramOne: ${paramOne} + param2: ${param2} = combination: ${combination}`
+);
+// Logs ==> ERROR! Uncaught ReferenceError: paramOne is not defined
